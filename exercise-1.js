c805ed2e8c6dc2ff1b009bce4fd865332821fe6e
PIPOAUMPHOL <aumpholllo@gmail.com>
2024-01-11 21:29:07 +0700

    feat: add complete exercise

--- exercise-1.js
+++ exercise-1.js
@@ -1,1 +1,10 @@
-// Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: "$credit_card_type",
+      total_by_payment: {
+        $sum: "$total_price",
+      },
+    },
+  },
+]);
